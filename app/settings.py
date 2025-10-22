7f2b809c508b8fabc6ed0ef50b54300fd4322dfa
David-Bonardi <david.bonardi@hotmail.com>
2025-10-22 16:37:18 -0300

    Config Allowed_hosts

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-b%d7ndb9+*o(u2599vzkb8-aue4ux2e=g+!#$y-ri3ev$l&8*e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
