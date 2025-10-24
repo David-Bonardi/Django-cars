04254ea6e2d221b5ad263d40dc5807ad83bd8846
David-Bonardi <david.bonardi@hotmail.com>
2025-10-23 23:21:06 -0300

    Configurando a responsividade da app

--- app/settings.py
+++ app/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
